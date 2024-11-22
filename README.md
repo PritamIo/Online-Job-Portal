67a234ec4adb801962aedeea222844f314ec9daa
Aryesh <aryeshsaha5302@gmail.com>
2024-11-22 18:46:33 +0530

    added firebase.config.js and env example

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Built Job Portal using ReactJS, Tailwind CSS and Firebase.
 
 - git clone <https-link> <folder-name>
 - run command 'npm install'
-- Create a firebase.config.js and put the firebase configuration inside that file
+- Create a firebase account and put the firebase configuration inside the firebase.config.js file
 - run command 'npm run dev' in terminal
 
 # Deploy Project on Vercel
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,7 +4,7 @@ function Navbar() {
     <div className='h-20 flex justify-between items-center w-full text-white px-8 md:px-40'>
         <div className='text-3xl font-bold'>JobPortal</div>
         <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-          <a href="/">Resume Builder</a>
+          <a href="https://minorclient.vercel.app/" target="_blank" rel="noopener noreferrer">Resume Builder</a>
         </button>
     </div>
   )
