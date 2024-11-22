fc30288c2ff4c525a387b0ccb8c559d8ad6abd41
Aryesh <aryeshsaha5302@gmail.com>
2024-11-22 19:45:41 +0530

    navbar resume-builder button fixed

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,13 +1,17 @@
-
 function Navbar() {
   return (
-    <div className='h-20 flex justify-between items-center w-full text-white px-8 md:px-40'>
-        <div className='text-3xl font-bold'>JobPortal</div>
-        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-          <a href="https://minorclient.vercel.app/" target="_blank" rel="noopener noreferrer">Resume Builder</a>
-        </button>
+    <div className="h-20 flex justify-between items-center w-full text-white px-8 md:px-40">
+      <div className="text-3xl font-bold">JobPortal</div>
+      <a
+        href="https://minorclient.vercel.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+      >
+        Resume Builder
+      </a>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
