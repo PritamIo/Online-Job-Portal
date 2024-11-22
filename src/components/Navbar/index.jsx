d6bc450cdecd6ea4e8accddb56cd0b558ed28a42
Aryesh <aryeshsaha5302@gmail.com>
2024-11-22 17:30:48 +0530

    search functionality improved and resume builder integrated

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
 
 function Navbar() {
   return (
-    <div className='h-20 flex items-center w-full text-white'>
-        <div className='text-3xl pl-20 font-bold'>JobPortal</div>
-
+    <div className='h-20 flex justify-between items-center w-full text-white px-8 md:px-40'>
+        <div className='text-3xl font-bold'>JobPortal</div>
+        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+          <a href="/">Resume Builder</a>
+        </button>
     </div>
   )
 }
